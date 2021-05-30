c0fcd7a0893acb61b7773d625edc1e9949531ed3
Alex Zhu <1224717847@qq.com>
2021-05-30 18:12:08 +0800

    pylint以及pep8支持

--- README.md
+++ README.md
@@ -1,8 +1,8 @@
 # Python 哔哩哔哩搜索爬虫
-1、需要本地配置浏览器驱动（mac下查看本地Chrome的版本，去http://npm.taobao.org/mirrors/chromedriver/ 下载对应版本chrome driver， 把下载好的Chrome driver放在  usr/local/bin）
+1、需要本地配置浏览器驱动（mac下查看本地Chrome的版本，去http://npm.taobao.org/mirrors/chromedriver/ 下载对应版本chrome driver， 把下载好的Chrome driver放在  /usr/local/bin）
 
 2、通过pip install selenium 安装selenium库
 
 3、运行该文件就可以在哔哩哔哩通过关键字搜索视频，将所有分页视频的(title, url)汇总成csv文件
 
-4、若要显式浏览器过程需将self.br = webdriver.Chrome(options=chrome_options) 换成self.br = webdriver.Chrome()
+4、若要显式浏览器过程需将self.driver = webdriver.Chrome(options=chrome_options) 换成self.driver = webdriver.Chrome()
--- download_bilibili_video.py
+++ download_bilibili_video.py
@@ -1,24 +1,51 @@
-from selenium import webdriver
-# from selenium.webdriver.common.keys import Keys
-from selenium.webdriver import ActionChains
-from selenium.webdriver.chrome.options import Options
+"""
+module docstring for download video urls
+from Bilibili_webpage
+"""
 import json
 import csv
 import time
 import urllib.parse
 import os.path
+from selenium import webdriver
+# from selenium.webdriver.common.keys import Keys
+from selenium.webdriver import ActionChains
+# from selenium.webdriver.chrome.options import Options
+from selenium.common.exceptions import NoSuchElementException
 
 
+def save_content_txt(contents, need_close=True):
+    """
+    save content text
+    :param contents:
+    :param need_close:
+    :return:
+    """
+    with open("B站视频资料.txt", "a") as file:
+        for content in contents:
+            json.dump(content, file, ensure_ascii=False, indent=2)
+            file.write("\n")
+        if need_close:
+            file.close()
 
-class Search_On_Bili(object):
 
+class SearchOnBili:
+    """
+    SearchOnBili docstring
+    """
     def __init__(self, keyword):
+        """
+        init
+        :param keyword:
+        """
+
         # PhantomJS() Selenium support for PhantomJS has been deprecated, please use headless
-        chrome_options = Options()
-        chrome_options.add_argument('--headless')
-        chrome_options.add_argument('--disable-gpu')
-        self.br = webdriver.Chrome(options=chrome_options)
-        # self.br = webdriver.Chrome()
+        # chrome_options = Options()
+        # chrome_options.add_argument('--headless')
+        # chrome_options.add_argument('--disable-gpu')
+        # self.driver = webdriver.Chrome(options=chrome_options)
+        self.driver = webdriver.Chrome()
+        # self.driver = webdriver.PhantomJS()
         if len(keyword) > 0:
             keyword = urllib.parse.quote(keyword, encoding='utf-8')
         self._url = "https://search.bilibili.com/all?keyword=" + keyword
@@ -26,99 +53,112 @@ class Search_On_Bili(object):
 
     @property
     def url(self):
+        """
+        @property
+        :return:
+        """
         return self._url
 
     def start_driver_and_begin_download(self, url):
-        self.br.get(url)
-        print("开始爬取url= ",url, "的资源")
+        """
+        init driver and begin download
+        :param url:
+        :return:
+        """
+        self.driver.get(url)
+        contents = self.get_contents()
+        print("开始爬取url= ", url, "的资源")
         print("开始爬取第一页")
-        contents = self.getContents()
         self.save_content_csv(contents, need_close=False)
-
+        # noinspection PyBroadException
         try:
-            while self.br.find_element_by_xpath('//li[@class="page-item next"]'):
-                next_btn = self.br.find_element_by_xpath('//li[@class="page-item next"]').find_element_by_xpath(
-                    "./button")
+            while self.driver.find_element_by_xpath(
+                    '//li[@class="page-item next"]'):
+                next_btn = self.driver.find_element_by_xpath(
+                    '//li[@class="page-item next"]').find_element_by_xpath("./button")
                 print("开始爬取下一页")
-                ActionChains(self.br).move_to_element(next_btn).perform()
-                if self.br.find_element_by_xpath('//div[@class="close"]'):
-                    close_btn = self.br.find_element_by_xpath('//div[@class="close"]')
-                    tip_style = self.br.find_element_by_xpath('//div[@class="lt-row"]'). \
-                        get_attribute("style")
+                ActionChains(self.driver).move_to_element(next_btn).perform()
+                if self.driver.find_element_by_xpath('//div[@class="close"]'):
+                    close_btn = self.driver.find_element_by_xpath(
+                        '//div[@class="close"]')
+                    tip_style = self.driver.find_element_by_xpath(
+                        '//div[@class="lt-row"]').get_attribute("style")
                     if tip_style.find("display: none;") == -1:
                         close_btn.click()
 
                 next_btn.click()
-                contents_new = self.getContents()
+                contents_new = self.get_contents()
                 self.save_content_csv(contents_new, need_close=False)
-
-            else:
-                self.save_content_csv([], need_close=True)
-                print("爬取完成url= ", url, "的资源")
-        except:
+        except NoSuchElementException:
             self.save_content_csv([], need_close=True)
             print("爬取完成url= ", url, "的资源")
 
-
-
-    def getContents(self):
+    def get_contents(self):
+        """
+        get content of element
+        :return:
+        """
         time.sleep(3)
-        li_list = self.br.find_elements_by_xpath('//ul[@class="video-list clearfix"]/li')
+        li_list = self.driver.find_elements_by_xpath(
+            '//ul[@class="video-list clearfix"]/li')
         contents = []
-        for li in li_list:  # 遍历所有项
+        for li_item in li_list:  # 遍历所有项
             item = {}
-            li_a_data = li.find_element_by_xpath('./a')
+            li_a_data = li_item.find_element_by_xpath('./a')
             item["title"] = li_a_data.get_attribute("title")  # 获取视频名字
             item["url"] = li_a_data.get_attribute("href")
             contents.append(item)
         return contents
 
-
-    def save_content_txt(self, contents, need_close=True):
-        f = open("B站视频资料.txt", "a")
-        for content in contents:
-            json.dump(content, f, ensure_ascii=False, indent=2)
-            f.write("\n")
-        if need_close:
-            f.close()
-
     def save_content_csv(self, contents, need_close=True):
+        """
+        save content to csv
+        :param contents:
+        :param need_close:
+        :return:
+        """
         path = r"./%s.csv" % self._keyword
         # if not os.path.exists(path):
-        f = open(path, "a", encoding='utf-8')
-        csv_writer = csv.writer(f)
-        # csv_writer.writerow(("title", "url"))
-        for content in contents:
-            csv_writer.writerow((content["title"], content["url"]))
-        if need_close:
-            f.close()
-            if os.path.exists(path):
-                try:
-                    new_path = urllib.parse.unquote(path)
-                    if new_path != path:
-                        os.rename(path, new_path)
-                        if os.path.exists(path):
-                            os.remove(path)
-                except:
-                    print("文件名替换失败")
-
-            else:
-                print("not exists")
+        with open(path, "a", encoding='utf-8') as file:
+            csv_writer = csv.writer(file)
+            # csv_writer.writerow(("title", "url"))
+            for content in contents:
+                csv_writer.writerow((content["title"], content["url"]))
+            if need_close:
+                file.close()
+                if os.path.exists(path):
+                    # noinspection PyBroadException
+                    try:
+                        new_path = urllib.parse.unquote(path)
+                        if new_path != path:
+                            os.rename(path, new_path)
+                            if os.path.exists(path):
+                                os.remove(path)
+                    except NotImplementedError:
+                        print("文件名替换失败")
+                else:
+                    print("not exists")
 
     def run(self):
+        """
+        run driver
+        :return:
+        """
         self.start_driver_and_begin_download(self.url)
         print("爬取完成")
-            
+
 
 if __name__ == '__main__':
     def input_method():
+        """
+        input keyword
+        :return:
+        """
         keyword = input("请输入查找关键字：")
         if len(keyword) > 0:
-            bili = Search_On_Bili(keyword)
+            bili = SearchOnBili(keyword)
             bili.run()
         else:
             input_method()
-
-
     while True:
         input_method()
